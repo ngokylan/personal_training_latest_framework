111ab49ebc2aed053992d231faca76c72205186d
ngokylan <n_mduc@yahoo.com>
2017-01-05 04:07:28 +1100

    Reduce Helper

--- es6/sections/reduce_helper/main.js
+++ es6/sections/reduce_helper/main.js
@@ -43,5 +43,82 @@ function balancedParens(string){
 	}, 0);
 }
 
-balancedParens("((((");
+balancedParens("(((())))");
 
+
+
+//////---------------------------
+//exercise 1
+/*
+Distance Traveled
+
+Use the 'reduce' helper to find the sum of all the distances traveled.  
+Assign the result to the variable 'totalDistance'
+*/
+var trips = [{ distance: 34 }, { distance: 12 } , { distance: 1 }];
+
+var totalDistance;
+totalDistance = trips.reduce(function(previous, trip){
+    return previous + trip.distance;
+}, 0);
+
+//exercise 2
+/*
+Reducing Properties
+
+Use the 'reduce' helper to create an object that tallies the number of sitting and standing desks.  
+The object returned should have the form '{ sitting: 3, standing: 2 }'.  
+The initial value has been provided to you.
+Hint: Don't forget to return the accumulator object (the first argument to the iterator function)
+*/
+var desks = [
+  { type: 'sitting' },
+  { type: 'standing' },
+  { type: 'sitting' },
+  { type: 'sitting' },
+  { type: 'standing' }
+];
+
+var deskTypes = desks.reduce(function(previous, desk) {
+    if(desk.type === "sitting"){ previous.sitting++;}
+    if(desk.type === "standing"){ previous.standing++;}
+    return previous;
+}, { sitting: 0, standing: 0 });
+
+
+
+//exercise 3
+/*
+Hardmode: Custom 'Unique' Helper
+
+Another really hard one!  Write a function called 'unique' 
+that will remove all the duplicate values from an array.
+
+For example, given the following array:
+var numbers = [1, 1, 2, 3, 4, 4];
+Your function should return
+[1, 2, 3, 4]
+Hint: Use the 'reduce' and 'find' helpers.  
+*/
+
+var numbers = [1, 1, 2, 3, 4, 4];
+
+
+function unique(array) {
+  return array.reduce(function(previous, arr){
+      if(previous.length > 0){
+          var is_duplicate = previous.find(function(prev){
+             return prev === arr; 
+          });
+          if(!is_duplicate){
+              previous.push(arr);
+          }
+      }else{
+          previous.push(arr);
+      }
+      return previous;
+  }, []);
+}
+
+//run the test
+unique(numbers);
