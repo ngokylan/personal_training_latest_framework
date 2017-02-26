0dc9f8767cec017c56860fd2efad49f41b5b8d5a
ngokylan <n_mduc@yahoo.com>
2017-02-27 10:25:14 +1100

    update

--- reactjs_redux/SimpleStarter_redux/src/actions/index.js
+++
@@ -1,9 +0,0 @@
-export function selectBook(book){
-	console.log('A book has been selected: ', book.title)
-	//selectBook is an ActionCreator, it needs to return an action,
-	//an object with a type property
-	return{
-		type: 'BOOK_SELECTED',
-		payload: book
-	}
-}
--- reactjs_redux/SimpleStarter_redux/src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function(){
-	return [
-		{title: 'Javascript: The good Parts', pages: 101},
-		{title: 'Harry Potter', pages: 39},
-		{title: 'The Dark Tower', pages: 85},
-		{title: 'Bamboo Heaven', pages: 1}
-	]
-}
